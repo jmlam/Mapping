21d16afcdfac8143f4fdede38823d930e1dd3f86
Jeremiah Lam <jiamialam@gmail.com>
2020-01-21 14:19:51 -0800

    added names to markers

--- mapping.py
+++ mapping.py
@@ -3,13 +3,14 @@ import pandas as pd
 volcanodata = pd.read_csv('Volcanoes.txt')
 lat = list(volcanodata['LAT'])
 lon = list(volcanodata['LON'])
+name = list(volcanodata['NAME'])
 
 map = folium.Map(location = [37.7749, -122.4194], zoom_start= 6, tiles = 'Stamen Terrain')
 
 fg = folium.FeatureGroup(name = 'My Map') #keeps all these features as one group
 
-for lat, lon in zip(lat, lon):
-    fg.add_child(folium.Marker(location = [lat, lon], popup = 'Volcano', icon = folium.Icon(color= 'green')))
+for lt, ln, nm in zip(lat, lon, name):
+    fg.add_child(folium.Marker(location = [lt, ln], popup = nm, icon = folium.Icon(color= 'green')))
 
 
 map.add_child(fg) # adds all features as group
