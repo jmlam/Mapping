3e3e48a750d1b7d8f51066c3409afd9ebe7d72ef
Jeremiah Lam <jiamialam@gmail.com>
2020-01-21 12:20:03 -0800

    added two new test markers

--- mapping.py
+++ mapping.py
@@ -1,3 +1,12 @@
 import folium
-map = folium.Map(location = [37.7749, -122.4194])
+map = folium.Map(location = [37.7749, -122.4194], zoom_start= 6, tiles = 'Stamen Terrain')
+
+fg = folium.FeatureGroup(name = 'My Map') #keeps all these features as one group
+
+coordlist = [[37.7749, -122.4194], [37.8, -122.4194]]
+for coordinate in coordlist:
+    fg.add_child(folium.Marker(location = coordinate, popup = 'Test', icon = folium.Icon(color= 'green')))
+
+
+map.add_child(fg) # adds all features as group
 map.save('Map1.html')
