833c360cae7d60d5797e5404399d8cd90e893a9e
Jeremiah Lam <jiamialam@gmail.com>
2020-01-21 14:17:08 -0800

    Added a volcano text list and marked all coordinates of volcanoes

--- mapping.py
+++ mapping.py
@@ -1,11 +1,15 @@
 import folium
+import pandas as pd
+volcanodata = pd.read_csv('Volcanoes.txt')
+lat = list(volcanodata['LAT'])
+lon = list(volcanodata['LON'])
+
 map = folium.Map(location = [37.7749, -122.4194], zoom_start= 6, tiles = 'Stamen Terrain')
 
 fg = folium.FeatureGroup(name = 'My Map') #keeps all these features as one group
 
-coordlist = [[37.7749, -122.4194], [37.8, -122.4194]]
-for coordinate in coordlist:
-    fg.add_child(folium.Marker(location = coordinate, popup = 'Test', icon = folium.Icon(color= 'green')))
+for lat, lon in zip(lat, lon):
+    fg.add_child(folium.Marker(location = [lat, lon], popup = 'Volcano', icon = folium.Icon(color= 'green')))
 
 
 map.add_child(fg) # adds all features as group
