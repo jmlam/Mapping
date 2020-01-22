db7983ca2a068543be17d81beef846e6b72fd718
Jeremiah Lam <jiamialam@gmail.com>
2020-01-22 12:25:13 -0800

    separated population and volcano feature groups

--- mapping.py
+++ mapping.py
@@ -8,7 +8,7 @@ elev = list(volcanodata['ELEV'])
 
 map = folium.Map(location = [37.7749, -122.4194], zoom_start= 6, tiles = 'Stamen Terrain') #map object
 
-fg = folium.FeatureGroup(name = 'My Map') #keeps all these features as one group
+fgv = folium.FeatureGroup(name = 'Volcanoes') #keeps all these features as one group
 
 def elevcolor(elev): #color codes markers
     'takes an elevation (int) and returns a color (str)'
@@ -20,19 +20,22 @@ def elevcolor(elev): #color codes markers
         return 'red'
 
 for lt, ln, nm, el in zip(lat, lon, name, elev): #volcano popups
-    fg.add_child(folium.CircleMarker(radius = 6, location = [lt, ln], popup = nm + '\n' + str(el)+ 'm', color= elevcolor(el), fill = True, fill_color = elevcolor(el), fill_opacity = .7)) #Circle markers
+    fgv.add_child(folium.CircleMarker(radius = 6, location = [lt, ln], popup = nm + '\n' + str(el)+ 'm', color= elevcolor(el), fill = True, fill_color = elevcolor(el), fill_opacity = .7)) #Circle markers
     #fg.add_child(folium.Marker(location = [lt, ln], popup = nm + '\n' + str(el)+ 'm', icon = folium.Icon(color= elevcolor(el)))) regular markers
 
+fgp = folium.FeatureGroup(name = 'Population')
+
 #adds country polygons
-fg.add_child(folium.GeoJson(data = open('world.json', 'r', encoding = 'utf-8-sig').read(),
+fgp.add_child(folium.GeoJson(data = open('world.json', 'r', encoding = 'utf-8-sig').read(),
 #geojson goes through all the features of each polygon
 #style function takes a function, lambda was given
 style_function = lambda x: {'fillColor': 'green' if x['properties']['POP2005'] <=10000000 else 
 'orange' if x['properties']['POP2005'] <= 20000000 else
 'red'}
 )) 
+map.add_child(fgv) 
+map.add_child(fgp) # adds all features as group
+map.add_child(folium.LayerControl())
 
 
-
-map.add_child(fg) # adds all features as group
 map.save('VolcanoMap.html')
