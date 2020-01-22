5ff4d697481b19e9c4344b5f33d2db2f36782596
Jeremiah Lam <jiamialam@gmail.com>
2020-01-22 10:39:50 -0800

    added function to dynamically add color to markers depending on volcano elevation

--- mapping.py
+++ mapping.py
@@ -10,8 +10,17 @@ map = folium.Map(location = [37.7749, -122.4194], zoom_start= 6, tiles = 'Stamen
 
 fg = folium.FeatureGroup(name = 'My Map') #keeps all these features as one group
 
+def elevcolor(elev): #color codes markers
+    'takes an elevation (int) and returns a color (str)'
+    if elev <= 2000:
+        return 'green'
+    elif elev <=3000:
+        return 'orange'
+    else:
+        return 'red'
+
 for lt, ln, nm, el in zip(lat, lon, name, elev):
-    fg.add_child(folium.Marker(location = [lt, ln], popup = nm + '\n' + str(el)+ 'm', icon = folium.Icon(color= 'green')))
+    fg.add_child(folium.Marker(location = [lt, ln], popup = nm + '\n' + str(el)+ 'm', icon = folium.Icon(color= elevcolor(el))))
 
 
 map.add_child(fg) # adds all features as group
