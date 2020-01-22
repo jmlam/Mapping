882205caba4d53cc141e5d860701d0889889cb2f
Jeremiah Lam <jiamialam@gmail.com>
2020-01-22 12:17:06 -0800

    color coded polygons

--- mapping.py
+++ mapping.py
@@ -23,8 +23,14 @@ for lt, ln, nm, el in zip(lat, lon, name, elev): #volcano popups
     fg.add_child(folium.CircleMarker(radius = 6, location = [lt, ln], popup = nm + '\n' + str(el)+ 'm', color= elevcolor(el), fill = True, fill_color = elevcolor(el), fill_opacity = .7)) #Circle markers
     #fg.add_child(folium.Marker(location = [lt, ln], popup = nm + '\n' + str(el)+ 'm', icon = folium.Icon(color= elevcolor(el)))) regular markers
 
-#adding world data
-fg.add_child(folium.GeoJson(data = (open('world.json', 'r', encoding = 'utf-8-sig').read()))) #adds country polygons
+#adds country polygons
+fg.add_child(folium.GeoJson(data = open('world.json', 'r', encoding = 'utf-8-sig').read(),
+#geojson goes through all the features of each polygon
+#style function takes a function, lambda was given
+style_function = lambda x: {'fillColor': 'green' if x['properties']['POP2005'] <=10000000 else 
+'orange' if x['properties']['POP2005'] <= 20000000 else
+'red'}
+)) 
 
 
 
